0179b8818a2066a1b0a44b57d5687021b03e2f7c
bradleyham <bradleyjamesham@gmail.com>
2023-02-06 20:18:55 +1300

    minimum viable product

--- dist/index.7c792717.js
+++ dist/index.7c792717.js
@@ -1,9 +1,18 @@
 const hamburger = document.getElementById("hamburger");
 const navRightSide = document.getElementById("nav-right-side");
+const mobileNav = document.getElementById("mobile-nav");
+let mobileListItems = document.querySelectorAll(".nav__list--mobile .nav__list--item");
 hamburger.addEventListener("click", (e)=>{
     e.preventDefault();
     hamburger.classList.toggle("is-active");
     navRightSide.classList.toggle("nav-active");
+});
+mobileListItems.forEach((item)=>{
+    item.addEventListener("click", (e)=>{
+        console.log("running");
+        hamburger.classList.toggle("is-active");
+        navRightSide.classList.toggle("nav-active");
+    });
 });
 
 //# sourceMappingURL=index.7c792717.js.map
