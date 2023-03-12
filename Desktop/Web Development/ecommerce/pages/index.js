0482493713dd10f30230fec1e68e24dbf51b840f
bradleyham <bradleyjamesham@gmail.com>
2023-03-12 19:31:33 +1300

    sanity and product component set up

---
+++ Desktop/Web Development/ecommerce/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { client } from '../lib/client'
+import { HeroBanner, FooterBanner, Product } from '../components'
+
+const Home = ({products, bannerData}) => {
+  return (
+    <>
+    {console.log(products)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      <div>
+        <h2 className='products-heading'>Best selling products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {
+          products?.map(product => <Product key={Product.id} product={product}/>)
+        }
+      </div>
+      <FooterBanner />
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+      props:{
+        products,
+        bannerData
+      }
+    }
+}
+
+export default Home
